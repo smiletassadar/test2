ed2651c4d8997692163e8d71832884bf8948ca5d
Lucky-PC\Lucky <frostycoder@gmail.com>
2019-07-13 13:02:21 +0300

    проверка создания чистой версии опенкарта 2.3.0.2

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test2/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/wamp/www/test2/catalog/');
+define('DIR_SYSTEM', 'D:/wamp/www/test2/system/');
+define('DIR_IMAGE', 'D:/wamp/www/test2/image/');
+define('DIR_LANGUAGE', 'D:/wamp/www/test2/catalog/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/test2/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/wamp/www/test2/system/config/');
+define('DIR_CACHE', 'D:/wamp/www/test2/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/test2/system/storage/download/');
+define('DIR_LOGS', 'D:/wamp/www/test2/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/wamp/www/test2/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/wamp/www/test2/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
